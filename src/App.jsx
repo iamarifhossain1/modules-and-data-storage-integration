fcd1de12cf6935f3705eba2e72cfe38a17f5611a
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-22 03:15:40 +0600

    es-6-export-import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
+import { add, divide as div, mult, sub } from './utils/math/math'
 
 function App() {
-
+  const sum = add(2,3);
+  const subtraction = sub(10,3);
+  const goonFol = mult(2,3);
+  const something = div(6,2);
+  console.log(sum,subtraction, goonFol, something);
+  
+  
   return (
     <>
       
